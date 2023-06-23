247bcc33a86da479ed0ca35289d06bfe81c47fc5
Chanachai Puttaruksa <chanachai_mig@hotmail.com>
2023-06-23 15:42:59 +0700

    initial commit

---
+++ src/main/java/com/example/mikaelpetclinicdemo/MikaelPetClinicDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mikaelpetclinicdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikaelPetClinicDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikaelPetClinicDemoApplication.class, args);
+	}
+
+}
