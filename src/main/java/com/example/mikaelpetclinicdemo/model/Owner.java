1863e735158fb25fcfbf8322b03a4e8de8b085ae
Chanachai Puttaruksa <chanachai_mig@hotmail.com>
2023-06-27 15:37:25 +0700

    modify some POJO. Closes #2.

--- src/main/java/com/example/mikaelpetclinicdemo/model/Owner.java
+++ src/main/java/com/example/mikaelpetclinicdemo/model/Owner.java
@@ -1,4 +1,5 @@
 package com.example.mikaelpetclinicdemo.model;
 
 public class Owner extends Person{
+
 }
--- src/main/java/com/example/mikaelpetclinicdemo/model/Vet.java
+++ src/main/java/com/example/mikaelpetclinicdemo/model/Vet.java
@@ -1,4 +1,5 @@
 package com.example.mikaelpetclinicdemo.model;
 
 public class Vet extends Person {
+
 }
